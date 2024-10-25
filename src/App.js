6524f61fc6245517e25822b62044f3658a09b8ce
pranjal254 <pranjalgaikwad254@gmail.com>
2024-10-25 16:50:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import InstantSplit from "./InstantSplit";
+import PayoutSimulator from "./PayoutSimulator";
+// Sampleg
+
+function App() {
+  return (
+    <div className="">
+      <PayoutSimulator/>
+    </div>
+  );
+}
+
+export default App;
